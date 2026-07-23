7aa523955218f017cdfb6a2ce3c25890167ee313
Jimmychuyucheng <164623139+Jimmychuyucheng@users.noreply.github.com>
2026-07-23 12:28:49 +0800

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # def dp[i]: fewest coins to save from 0 to i
+        # dp[i] = max(dp[i-coin] for coin in coins)
+        dp = [float("inf") for _ in range(amount+1)]
+        dp[0] = 0
+    
+        for i in range(amount+1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], dp[i-coin]+1)
+
+
+        return dp[amount] if dp[amount]!=float("inf") else -1
